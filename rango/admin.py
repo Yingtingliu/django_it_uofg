4165685c2b98e70d1032824d42a744866bdd213e
Ying Ting Liu <2817385L@student.gla.ac.uk>
2023-01-30 13:21:26 +0000

    chapter 5 commit

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,40 @@
-from django.contrib import admin
+import datetime
 
-# Register your models here.
+from django.contrib import admin
+from rango.models import Category, Page
+from django.utils import timezone
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+
+admin.site.register(Category)
+admin.site.register(Page, PageAdmin)
+
+
+
+# https://docs.djangoproject.com/en/2.1/ref/contrib/admin/
+
+from .models import Choice, Question
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
+
+admin.site.register(Question, QuestionAdmin)
--- rango/models.py
+++ rango/models.py
@@ -1,11 +1,22 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+    # typo within the admin interface (Categorys, not Categories) can be fixed by adding a nested Meta class 
+    # into your model definitions with the verbose_name_plural attribute.
+    class Meta:
+        verbose_name_plural = 'Categories'
+
     def __str__(self):
         return self.name
+
 class Page(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     title = models.CharField(max_length=128)
@@ -13,3 +24,20 @@ class Page(models.Model):
     views = models.IntegerField(default=0)
     def __str__(self):
         return self.title
+
+# p.83 Official Tutorial
+# https://docs.djangoproject.com/en/2.1/intro/tutorial02/
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    def __str__(self):
+        return self.question_text
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.choice_text
