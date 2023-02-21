a1e4c04584436728d43e22614829fc8ba6fe3b05
Ying Ting Liu <2817385L@student.gla.ac.uk>
2023-02-21 16:23:34 +0000

    test

--- rango/views.py
+++ rango/views.py
@@ -17,7 +17,8 @@ def index(request):
     # Retrieve the top 5 only -- or all if less than 5.
     # Place the list in our context_dict dictionary (with our boldmessage!)
     # that will be passed to the template engine.
-    category_list = Category.objects.order_by('-likes')[:5]
+    
+    category_list = Category.objects.order_by('-likes')[:5] 
     context_dict = {}
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
