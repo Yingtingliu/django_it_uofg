d118d52b5e63d7117dcfe2bc543d7b56464a591e
Ying Ting Liu <2817385L@student.gla.ac.uk>
2023-03-18 23:05:33 +0000

    chapter 7

--- rango/views.py
+++ rango/views.py
@@ -6,7 +6,7 @@ from rango.models import Page
 # ch7
 from rango.forms import CategoryForm
 from django.shortcuts import redirect
-
+from django.template.loader import get_template, select_template
 
 def index(request):   
 
@@ -26,7 +26,7 @@ def index(request):
     that will be passed to the template engine.
     """
     
-    category_list = Category.objects.order_by('-likes')[:5] 
+    category_list = Category.objects.order_by('-likes')[:10] # this shows the top 10 categories
     context_dict = {}
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
@@ -64,22 +64,33 @@ def show_category(request, category_name_slug):
     return render(request, 'rango/category.html', context=context_dict)
 
 # ch7 forms: p.116
+# def add_category(request):
+#     form = CategoryForm()
+#     # A HTTP POST?
+#     if request.method == 'POST':
+#         form = CategoryForm(request.POST)
+#         # Have we been provided with a valid form?
+#     if form.is_valid():
+#         # Save the new category to the database.
+#         form.save(commit=True)
+#         # Now that the category is saved, we could confirm this.
+#         # For now, just redirect the user back to the index view.
+#         return redirect('/rango/')
+#     else:
+#     # The supplied form contained errors 
+#     # just print them to the terminal.
+#         print(form.errors)
+#     # Will handle the bad form, new form, or no form supplied cases.
+#     # Render the form with error messages (if any).
+#     return render(request, 'rango/rango/add_category.html', {'form': form})
+
 def add_category(request):
     form = CategoryForm()
-    # A HTTP POST?
+
     if request.method == 'POST':
         form = CategoryForm(request.POST)
-        # Have we been provided with a valid form?
-    if form.is_valid():
-        # Save the new category to the database.
-        form.save(commit=True)
-        # Now that the category is saved, we could confirm this.
-        # For now, just redirect the user back to the index view.
-        return redirect('/rango/')
-    else:
-    # The supplied form contained errors 
-    # just print them to the terminal.
-        print(form.errors)
-    # Will handle the bad form, new form, or no form supplied cases.
-    # Render the form with error messages (if any).
+        if form.is_valid():
+            form.save(commit=True)
+            return redirect('/rango/')
+    
     return render(request, 'rango/add_category.html', {'form': form})
