aa4f29cbe6b3d2441b1376220da85163c27de1cb
Ying Ting Liu <2817385L@student.gla.ac.uk>
2023-03-20 03:43:48 +0000

    chapter 13 adding bing search

--- rango/views.py
+++ rango/views.py
@@ -15,6 +15,8 @@ from django.shortcuts import render
 from .forms import UserForm, UserProfileForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+# ch13
+from rango.bing_search import run_query
 
 def index(request):
     """
@@ -167,3 +169,12 @@ def visitor_cookie_handler(request):
         request.session['last_visit'] = last_visit_cookie
     # Update/set the visits cookie
     request.session['visits'] = visits
+
+def search(request):
+    result_list = []
+    if request.method == 'POST':
+        query = request.POST['query'].strip()
+    if query:
+    # Run our Bing function to get the results list!
+        result_list = run_query(query)
+    return render(request, 'rango/search.html', {'result_list': result_list})
