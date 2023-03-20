f4b701d0cd5cf6daf0f4b92979679166d77eaf65
Ying Ting Liu <2817385L@student.gla.ac.uk>
2023-03-20 00:20:30 +0000

    chapter 11 using Redux

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rango',
-    'rango.templatetags'
+    'rango.templatetags',
+    'registration', # django-registration-redux
 ]
 
 MIDDLEWARE = [
@@ -138,4 +139,14 @@ PASSWORD_HASHERS = [
 'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
 ]
 
-LOGIN_URL = 'rango:login'
+# LOGIN_URL = 'rango:login'
+
+# If True, users can register.
+REGISTRATION_OPEN = True
+# If True, the user will be automatically logged in after registering.
+REGISTRATION_AUTO_LOGIN = True
+# The URL that Django redirects users to after logging in.
+LOGIN_REDIRECT_URL = 'rango:index'
+# The page users are directed to if they are not logged in.
+# This was set in a previous chapter. The registration package uses this, too.
+LOGIN_URL = 'auth_login'
