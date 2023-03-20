63ad0046700faf3ef607ef593524e785de337655
Ying Ting Liu <2817385L@student.gla.ac.uk>
2023-03-20 03:43:48 +0000

    chapter 13 adding bing search

--- rango/urls.py
+++ rango/urls.py
@@ -29,6 +29,7 @@ urlpatterns = [
     # chaper 7 excerise   
     path('category/<slug:category_name_slug>/add_page/', views.add_page, name='add_page'),
     path('restricted/', views.restricted, name='restricted'),
+    path('search/', views.search, name='search'),
 
 ] 
 
